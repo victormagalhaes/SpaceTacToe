d17c3721483a40fcdaa1f6d894f69f5fe6e2632a
Victor Magalhães <aheia1412@gmail.com>
2017-06-21 12:02:59 -0700

    Finishing logic

--- NeonTacToe/Program.cs
+++ NeonTacToe/Program.cs
@@ -6,60 +6,67 @@ namespace NeonTacToe
     {
         static void Main(string[] args)
         {
-            TicTacToe tGame = new TicTacToe(sMatrixPositions: { "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+            TicTacToe tGame = new TicTacToe(sMatrixPositions: new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+            bool bTheGameIsNotOver = true;
 
-            tGame.CreateMatrix(position: 0, );
-            string[,] sMatrixTicTacToe = CreateMatrix(sMatrixByPosition);
-            ShowMatrix(sMatrixTicTacToe);
+            tGame.ShowMatrix();
 
-            Console.ReadKey();
-
-            sMatrixByPosition[4] = "X";
-            sMatrixTicTacToe = CreateMatrix(sMatrixByPosition);
-            ShowMatrix(sMatrixTicTacToe);
-        }
-
-        private static void ShowMatrix(string[,] sMatrixTicTacToe)
-        {
-            int iCounter = 1;
-
-            Console.Clear();
-            foreach (string row in sMatrixTicTacToe)
+            while (bTheGameIsNotOver)
             {
-                if (iCounter % 3 == 0)
-                {
-                    Console.WriteLine(row);
-                }
-                else
-                {
-                    Console.Write(row);
-                }
+                tGame.GetInputFirstPlayer();
+                tGame.ShowMatrix();
+                bTheGameIsNotOver = tGame.VerifyGame();
 
-                iCounter++;
+                if (bTheGameIsNotOver)
+                {
+					tGame.GetInputSecondPlayer();
+					tGame.ShowMatrix();
+					bTheGameIsNotOver = tGame.VerifyGame();
+                }
             }
         }
 
-        private static string[,] CreateMatrix(string[] sMatrixByPosition)
-        {
-            string[,] sMatrixTicTacToe = {
-                { " ------ ", " ------ ", " ------ " },
-                { "|      |", "|      |", "|      |" },
-                { $"|   { sMatrixByPosition[0] }  |", $"|   { sMatrixByPosition[1] }  |", $"|   { sMatrixByPosition[2] }  |" },
-                { "|      |", "|      |", "|      |" },
-                { " ------ ", " ------ ", " ------ " },
-                { " ------ ", " ------ ", " ------ " },
-                { "|      |", "|      |", "|      |" },
-                { $"|   { sMatrixByPosition[3] }  |", $"|   { sMatrixByPosition[4] }  |", $"|   { sMatrixByPosition[5] }  |" },
-                { "|      |", "|      |", "|      |" },
-                { " ------ ", " ------ ", " ------ " },
-                { " ------ ", " ------ ", " ------ " },
-                { "|      |", "|      |", "|      |" },
-                { $"|   { sMatrixByPosition[6] }  |", $"|   { sMatrixByPosition[7] }  |", $"|   { sMatrixByPosition[8] }  |" },
-                { "|      |", "|      |", "|      |" },
-                { " ------ ", " ------ ", " ------ " }
-            };
+        //private static void ShowMatrix(string[,] sMatrixTicTacToe)
+        //{
+        //    int iCounter = 1;
 
-            return sMatrixTicTacToe;
-        }
+        //    Console.Clear();
+        //    foreach (string row in sMatrixTicTacToe)
+        //    {
+        //        if (iCounter % 3 == 0)
+        //        {
+        //            Console.WriteLine(row);
+        //        }
+        //        else
+        //        {
+        //            Console.Write(row);
+        //        }
+
+        //        iCounter++;
+        //    }
+        //}
+
+        //private static string[,] CreateMatrix(string[] sMatrixByPosition)
+        //{
+        //    string[,] sMatrixTicTacToe = {
+        //        { " ------ ", " ------ ", " ------ " },
+        //        { "|      |", "|      |", "|      |" },
+        //        { $"|   { sMatrixByPosition[0] }  |", $"|   { sMatrixByPosition[1] }  |", $"|   { sMatrixByPosition[2] }  |" },
+        //        { "|      |", "|      |", "|      |" },
+        //        { " ------ ", " ------ ", " ------ " },
+        //        { " ------ ", " ------ ", " ------ " },
+        //        { "|      |", "|      |", "|      |" },
+        //        { $"|   { sMatrixByPosition[3] }  |", $"|   { sMatrixByPosition[4] }  |", $"|   { sMatrixByPosition[5] }  |" },
+        //        { "|      |", "|      |", "|      |" },
+        //        { " ------ ", " ------ ", " ------ " },
+        //        { " ------ ", " ------ ", " ------ " },
+        //        { "|      |", "|      |", "|      |" },
+        //        { $"|   { sMatrixByPosition[6] }  |", $"|   { sMatrixByPosition[7] }  |", $"|   { sMatrixByPosition[8] }  |" },
+        //        { "|      |", "|      |", "|      |" },
+        //        { " ------ ", " ------ ", " ------ " }
+        //    };
+
+        //    return sMatrixTicTacToe;
+        //}
     }
 }
--- NeonTacToe/TicTacToe.cs
+++ NeonTacToe/TicTacToe.cs
@@ -1,32 +1,31 @@
-﻿namespace NeonTacToe
+﻿using System;
+
+namespace NeonTacToe
 {
     class TicTacToe
     {
         private string[] m_sMatrixPositions;
-        private string[,] m_sMatrixTacToe;
+        private int[] m_iPlayablePositions;
+        private int m_iNumberOfTurns;
 
-        public TicTacToe(string[] sMatrixPositions)
+		public TicTacToe(string[] sMatrixPositions)
+		{
+			this.m_sMatrixPositions = sMatrixPositions;
+			this.m_iPlayablePositions = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            this.m_iNumberOfTurns = 0;
+		}
+
+        public int INumberOfTurns
         {
-            this.m_sMatrixPositions = sMatrixPositions;
-            this.m_sMatrixTacToe = {
-                { " ------ ", " ------ ", " ------ " },
-                { "|      |", "|      |", "|      |" },
-                { $"|   { sMatrixPositions[0] }  |", $"|   { sMatrixPositions[1] }  |", $"|   { sMatrixPositions[2] }  |" },
-                { "|      |", "|      |", "|      |" },
-                { " ------ ", " ------ ", " ------ " },
-                { " ------ ", " ------ ", " ------ " },
-                { "|      |", "|      |", "|      |" },
-                { $"|   { sMatrixPositions[3] }  |", $"|   { sMatrixPositions[4] }  |", $"|   { sMatrixPositions[5] }  |" },
-                { "|      |", "|      |", "|      |" },
-                { " ------ ", " ------ ", " ------ " },
-                { " ------ ", " ------ ", " ------ " },
-                { "|      |", "|      |", "|      |" },
-                { $"|   { sMatrixPositions[6] }  |", $"|   { sMatrixPositions[7] }  |", $"|   { sMatrixPositions[8] }  |" },
-                { "|      |", "|      |", "|      |" },
-                { " ------ ", " ------ ", " ------ " }
-            };
+            get
+            {
+                return m_iNumberOfTurns;
+            }
 
-            CreateMatrix(SMatrixPositions);
+			set
+			{
+				m_iNumberOfTurns = value;
+			}
         }
 
         public string[] SMatrixPositions
@@ -42,22 +41,143 @@
             }
         }
 
-        public string[,] SMatrixTacToe
+		public int[] IPlayablePositions
+		{
+			get
+			{
+				return m_iPlayablePositions;
+			}
+
+			set
+			{
+				m_iPlayablePositions = value;
+			}
+		}
+
+        public bool VerifyGame()
         {
-            get
+            INumberOfTurns++;
+            int iWinnerPosition = -1;
+			if ((SMatrixPositions[0] == SMatrixPositions[1] && SMatrixPositions[1] == SMatrixPositions[2])
+			|| (SMatrixPositions[0] == SMatrixPositions[4] && SMatrixPositions[4] == SMatrixPositions[8])
+			|| (SMatrixPositions[0] == SMatrixPositions[3] && SMatrixPositions[3] == SMatrixPositions[6]))
+			{
+				iWinnerPosition = 0;
+			}
+			else if (SMatrixPositions[1] == SMatrixPositions[4] && SMatrixPositions[4] == SMatrixPositions[7])
+			{
+				iWinnerPosition = 1;
+			}
+			else if ((SMatrixPositions[2] == SMatrixPositions[4] && SMatrixPositions[4] == SMatrixPositions[6])
+					|| (SMatrixPositions[2] == SMatrixPositions[5] && SMatrixPositions[5] == SMatrixPositions[8]))
+			{
+				iWinnerPosition = 2;
+			}
+			else if (SMatrixPositions[3] == SMatrixPositions[4] && SMatrixPositions[4] == SMatrixPositions[5])
+			{
+				iWinnerPosition = 3;
+			}
+			else if (SMatrixPositions[6] == SMatrixPositions[7] && SMatrixPositions[7] == SMatrixPositions[8])
+			{
+				iWinnerPosition = 6;
+			}
+
+            try
             {
-                return m_sMatrixTacToe;
+				if (SMatrixPositions[iWinnerPosition] == "X")
+				{
+					Console.WriteLine("Player 1 wins the game!");
+					return false;
+				}
+				else if (SMatrixPositions[iWinnerPosition] == "O")
+				{
+					Console.WriteLine("Player 2 wins the game!");
+					return false;
+				}
+            }
+            catch (Exception ex)
+            {
+                if (INumberOfTurns == 9)
+                {
+					Console.WriteLine("It's a draw!");
+					return false;
+                }
             }
 
-            set
+            return true;
+        }
+
+        public void GetInputFirstPlayer()
+        {
+            while (true)
             {
-                m_sMatrixTacToe = value;
+				Console.WriteLine("First player, choose your position [1 - 9]");
+				int iInputInteger;
+				string sUserInput = Console.ReadLine();
+				bool bIsActionValid = int.TryParse(sUserInput, out iInputInteger);
+
+				try
+                {
+					if (bIsActionValid && IPlayablePositions[iInputInteger - 1] == 0)
+					{
+						IPlayablePositions[iInputInteger - 1] = 1;
+						break;
+					}
+                    else
+                    {
+                        Console.WriteLine("Ops. You typed something strange. Try again.");
+                    }
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Ops. You typed something strange. Try again.");
+				}
+
             }
         }
 
-        public void CreateMatrix(string[] sMatrixByPosition)
+		public void GetInputSecondPlayer()
+		{
+			while (true)
+			{
+				Console.WriteLine("Second player, choose your position [1 - 9]");
+				int iInputInteger;
+				string sUserInput = Console.ReadLine();
+				bool bIsActionValid = int.TryParse(sUserInput, out iInputInteger);
+
+				try
+				{
+					if (bIsActionValid && IPlayablePositions[iInputInteger - 1] == 0)
+					{
+						IPlayablePositions[iInputInteger - 1] = 2;
+						break;
+					}
+					else
+					{
+						Console.WriteLine("Ops. You typed something strange. Try again.");
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("Ops. You typed something strange. Try again.");
+				}
+
+			}
+		}
+
+        public void ShowMatrix()
         {
-            SMatrixTacToe = {
+            for (int i = 0; i < IPlayablePositions.Length; i++)
+            {
+                if (IPlayablePositions[i] == 1)
+                {
+                    SMatrixPositions[i] = "X";
+                }
+                else if (IPlayablePositions[i] == 2)
+                {
+                    SMatrixPositions[i] = "O";
+                }
+            }
+            string[,] sMatrixTacToe = {
                 { " ------ ", " ------ ", " ------ " },
                 { "|      |", "|      |", "|      |" },
                 { $"|   { SMatrixPositions[0] }  |", $"|   { SMatrixPositions[1] }  |", $"|   { SMatrixPositions[2] }  |" },
@@ -74,6 +194,23 @@
                 { "|      |", "|      |", "|      |" },
                 { " ------ ", " ------ ", " ------ " }
             };
+
+            int iCounter = 1;
+
+            Console.Clear();
+            foreach (string row in sMatrixTacToe)
+            {
+                if (iCounter % 3 == 0)
+                {
+                    Console.WriteLine(row);
+                }
+                else
+                {
+                    Console.Write(row);
+                }
+
+                iCounter++;
+            }
         }
     }
 }
